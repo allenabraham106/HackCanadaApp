20634def260c6d25b6be90653f1fbcbfea88bb07
Allen Abraham <allenabraham106@gmail.com>
2026-03-06 22:01:08 -0500

    add xcode project

---
+++ Behaviourly/Behaviourly/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Behaviourly
+//
+//  Created by Allen Abraham on 2026-03-06.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
